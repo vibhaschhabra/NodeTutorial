92904bc16c5aaf0548ebc77019e7a62e2152ebcd
Vibhas Chhabra <vibhas123@gmail.com>
2025-08-13 20:53:26 +0530

    Initial Commit of NodeTutorial Project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname,'public')));
+// Handle GET requests to the root URL
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','about.html'));
+})
+app.listen(PORT,()=>{
+    console.log("Server is running on http://localhost:" + PORT);
+});
