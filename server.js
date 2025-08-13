8de712a6b7d71dc40072b2bd2c1573cf3d61ef8d
Vibhas Chhabra <vibhas123@gmail.com>
2025-08-13 23:06:59 +0530

    one more change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended: true}));
 app.get('/',(req,res)=>{
