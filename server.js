d23389c552e862d4cfa49ec7e93d46472997a93b
Vibhas Chhabra <vibhas123@gmail.com>
2025-08-13 23:00:06 +0530

    Some Server files are updated and also contact form is updated

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-// Serve static files from the 'public' directory
+
 app.use(express.static(path.join(__dirname,'public')));
-// Handle GET requests to the root URL
+app.use(express.urlencoded({extended: true}));
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname,'public','index.html'));
 });
 app.get('/about',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','about.html'));
-})
+});
+app.post('/submit-form',(req,res)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const message = req.body.message;
+    console.log(`Recieved form submission:${name}`);
+    res.send(`Thanks ${name}, your message has been received!,Your Email ID is ${email}, Subject is ${subject}, and your message is ${message}.`);
+});
 app.listen(PORT,()=>{
-    console.log("Server is running on http://localhost:" + PORT);
+    console.log('Server is running on http://Localhost:' + PORT);
 });
